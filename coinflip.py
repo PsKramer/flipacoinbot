1d7a3c480f219203e93da3fb42556a0f4a61a895
Payton Kramer <payton@apexhorizon.net>
2016-12-18 08:03:29 -0800

    Update coinflip.py

--- coinflip.py
+++ coinflip.py
@@ -1,24 +1,42 @@
 import praw, random
 
-TRIGGERS = ['flip a coin', 'coinflip', 'flipacoin', 'heads or tails']
-REPLY_TEMP = 'As you requested, I flipped a coin for you, the result was **{}** \n\n --- \n\n ^^For ^^more ^^information/to ^^complain ^^about ^^me, ^^see ^^/r/flipacoinbot'
+# Start Vars
+TRIGGERS = ['flip a coin', 'coinflip', 'flipacoin', 'heads or tails']  #What the bot will look for in comments
+REPLY_TEMP = 'As you requested, I flipped a coin for you, the result was **{}** \n\n --- \n\n ^^For ^^more ^^information/to ^^complain ^^about ^^me, ^^see ^^/r/flipacoinbot'  #Reply template, with heads/tails being placed in {}
 SIDES = ['Heads', 'Tails']
+SUBS = 'all-askreddit-suicidewatch'  #The subreddit(s) that the bot will search in
+
+# Login details
+ID = ''
+SECRET = ''
+AGENT = 'a coinflipping script'
+USER = 'flipacoinbot'
+PASS = 'hunter2'
+# End Vars
+
+def bot():
+    try:
+        main()
+    except:
+        print('error, restarting')
+        main()  #ignore exceptions by restarting
+
 
 def main():
-	reddit = praw.Reddit(client_id='[id]',
-	                     client_secret='[secret]',
-	                     user_agent='a coin flipping script',
-	                     username='flipacoinbot',
-	                     password='hunter2')
-	subreddit = reddit.subreddit('all-askreddit-suicidewatch')
+	reddit = praw.Reddit(client_id=ID,
+	                     client_secret=SECRET',
+	                     user_agent=AGENT,
+	                     username=USER,
+	                     password=PASS)
+	subreddit = reddit.subreddit(SUBS)
 
 	for comment in subreddit.stream.comments():
 		proccess(comment)
 
 def proccess(comment):
-	normal_comment = comment.body.lower()
+	normal_comment = comment.body.lower()   #make all comments lowercase to make easier proccessing
 	for  call in TRIGGERS:
-		if call in normal_comment and comment.author != 'flipacoinbot':
+		if call in normal_comment and comment.author != USER:
 			reply_text = REPLY_TEMP.format(random.choice(SIDES))
 			comment.reply(reply_text)
 
@@ -26,8 +44,4 @@ def proccess(comment):
 
 
 while True:
-    try:
-        main()
-    except:
-        print('error, restarting')
-        main()
+    bot()
